8444aa178005e993bbaed68eff72b4d794e07b1c
ziiqii <ziiiqiii@gmail.com>
2024-03-21 13:32:05 +0800

    Modify isSamePerson
    isSamePerson now checks name, phone number and email

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -62,7 +62,8 @@ public class Person {
     }
 
     /**
-     * Returns true if both persons have the same name.
+     * Returns true if both persons have the same name, phone, and email.
+     * This is because phone and email should be unique to each person.
      * This defines a weaker notion of equality between two persons.
      */
     public boolean isSamePerson(Person otherPerson) {
@@ -71,7 +72,9 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && otherPerson.getName().equals(getName())
+                && phone.equals(otherPerson.phone)
+                && email.equals(otherPerson.email);
     }
 
     /**
